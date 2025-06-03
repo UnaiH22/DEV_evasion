f83e4ed7267c4ae0019c502e976ccffb37529cb4
UnaiH22 <53757754+UnaiH22@users.noreply.github.com>
2025-06-03 17:41:36 +0200

    Add files via upload

---
+++ detectarVM.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void check_virtualization() {
+    char buffer[128];
+    FILE *fp = popen("wmic computersystem get manufacturer", "r");  // Ejecuta comando en Windows
+    
+    if (fp == NULL) {
+        printf("Error al ejecutar el comando.\n");
+        return;
+    }
+
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        if (strstr(buffer, "VMware") || strstr(buffer, "VirtualBox") || strstr(buffer, "QEMU")) {
+            printf("Máquina Virtual detectada. Cerrando programa...\n");
+            exit(0);
+        }
+    }
+
+    pclose(fp);
+    printf("No se ha detectado ninguna Máquina Virtual.\n");
+}
+
+int main() {
+    check_virtualization();
+    return 0;
+}
